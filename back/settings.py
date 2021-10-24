18752d625e28679aff02350ed1c894acf8836182
FlorianMrn <fmerrien.dev@gmail.com>
2021-10-24 18:03:18 +0200

    make it better

--- back/settings.py
+++ back/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +29,7 @@ DB_USER = str(os.getenv('DB_USER'))
 DB_NAME = str(os.getenv('DB_NAME'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['cryptotrackerstudi.herokuapp.com']
 
@@ -190,3 +191,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
